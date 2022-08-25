45b981b95893f6f60c10833dfccbb5033c7a19e3
Igor Adulyan <53629576+igoradulian@users.noreply.github.com>
2022-08-25 13:16:08 -0700

    This is Spring Boot master branch wich contian blank project for cloning and set up. Project created as a guide and demo for learning purposes. * Who can use it? These examples are useful both for beginners with no previous experience with Spring and for individuals who good knowlgde of Spring Boot. However, Java programing basic knowldge needed. Instructors may use these examples for teaching purposes.
    The project is divided into the following branches:
    
    1) Spring MVC and Timeleaf
    2) Spring Date and Spring MVC
    3) Spring Security
    4) Spring Microservices
    
    To move from one topic to another, the user needs to switch to the
    branch of interest to him / her
    
    * Installation:
    
    1) For a successful installation of the project, it recommended cloning
       the project using the clone button.
    2) Next use "git clone <link>" in your terminal to clone poject in
       desired directory

---
+++ src/main/java/com/learning/demo/SpringDemoLearningApplication.java
@@ -0,0 +1,13 @@
+package com.learning.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDemoLearningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/learning/demo/SpringDemoLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
